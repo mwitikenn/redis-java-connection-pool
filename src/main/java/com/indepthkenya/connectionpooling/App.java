39367a8948ea5661a343e033b6caac14b2e772d3
Mwiti_Kennedy <ken@africomltd.com>
2020-10-02 10:14:37 +0300

    initial commit

---
+++ src/main/java/com/indepthkenya/connectionpooling/App.java
@@ -0,0 +1,13 @@
+package com.indepthkenya.connectionpooling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
